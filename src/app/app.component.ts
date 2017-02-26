a33c7596129711ce45e0a9db04e5d21e275f08ca
Aaron Todd <toddaa@gmail.com>
2017-02-25 19:42:25 -0500

    Adds header, ready for input

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
+	moduleId: module.id,
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+  title = 'Nagios Wallboard'; 
+  icon = 'fa-heartbeat';
+
+  apiURL = 'http://10.127.11.17:8080/state';
+
+  overviewOk: number;
+  overviewWarning: number;
+  overviewCritical: number;
+  overviewUnknown: number;
+	errorMessage: string;
+
+  nagHostsIssue: any[] = [];
+	nagHostsOK: any[] = [];
+
+  constructor() { }
+
+  ngOnInit() {
+    
+  }
 }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,11 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import {Observable} from 'rxjs/Rx';
+
+//import { NagiosService } from '../host/nagios.service';
 
 @Component({
+	moduleId: module.id,
   selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  templateUrl: 'header.component.html',
+  styleUrls: ['header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  //title = 'LEIGHTRONIX Monitoring Dashboard - Component LEVEL'; 
+  @Input() appTitle: string;
+  @Input() appIcon: string;
+
+
+  @Input() overviewOk: number;
+  @Input() overviewWarning: number;
+  @Input() overviewCritical: number;
+  @Input() overviewUnknown: number;
+	errorMessage: string;
+  
+
+  clock = Observable
+    .interval(1000)
+    .map(()=> new Date());
 
   constructor() { }
 
